0960f5e0fa35ccd9e9d49598e115752cb47d1ac7
Parth Puranik <thosparth@gmail.com>
2024-02-21 21:52:10 +0530

    Backend setup done

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const port = 5000;
+const mongoDB = require("./db");
+mongoDB();
+const JWT_SECRET = "MYNAMEISPARTHANDAMAWESOMEHEREIAM"
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/", require("./Routes/userfunction"));
+app.use("/", require("./Routes/tasks"));
+app.use("/",require("./Routes/spotify"));
+
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
