2dc3c58d2f52762b090137201169f635fb697d23
Tanishqa <sawant.tanishqa@gmail.com>
2024-04-12 01:02:46 +0530

    index updated for case routes

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ app.get("/", (req, res) => {
 
 app.use("/govt", require("./Routes/govt"))
 app.use("/student", require("./Routes/student"))
+app.use("/cases", require("./Routes/case"))
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`);
